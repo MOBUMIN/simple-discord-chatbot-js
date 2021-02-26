f018c7c3fbfca2b27afdf558d6d5f370f59612df
MOBUMIN <kiju23@naver.com>
2021-02-26 21:55:37 +0900

    투표 봇 완성
    간단한 투표 봇!

--- src/eventHandlers/onMessage.js
+++ src/eventHandlers/onMessage.js
@@ -1,5 +1,48 @@
-function onMessage(message) {
-  console.log("onMessage!");
+import { MessageEmbed } from "discord.js";
+import axios from 'axios';
+
+async function onMessage(message) {
+	const prefix = "!";
+	if (message.author.bot === true || !message.content.startsWith(prefix)) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/); // regular expression
+	const command = args.shift().toLowerCase();
+	
+	if (command === "안녕"){
+		message.reply(' 조용히해');
+	}
+
+	if (command === "투표"){
+		const voteEmojis = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣"];
+		const question = args.shift();
+		if (args.length < 1 || args.length > 5) {
+			message.reply("선택 항목은 1-5개만 지원합니다.");
+			return;
+		}
+
+		let description = "";
+		args.forEach((arg, i) => {
+			description += `${voteEmojis[i]}: ${args[i]}\n`;
+		});
+
+
+		const embed = new MessageEmbed(); // 삽입 툴
+		embed.setTitle(question);
+		embed.setDescription(description);
+
+		const vote = await message.reply("투표해주세요", { embed: embed });
+		args.forEach(async (arg, i) => {
+			await vote.react(voteEmojis[i]);
+		  });
+
+	}
+
+	if (command === "강아지") {
+		const response = await axios.get("https://dog.ceo/api/breeds/image/random")
+		.then((response)=>{
+			message.reply(response.data.message);
+		})
+	}
 }
 
 export default onMessage;
--- src/eventHandlers/onceReady.js
+++ src/eventHandlers/onceReady.js
@@ -1,5 +1,22 @@
-function onceReady(client) {
+import isUp from 'is-up';
+
+async function onceReady(client) {
   console.log("onceReady!");
+
+  setInterval(async () => {
+    checkWebStatus(client);
+  }, 5 * 1000); // 5초에 한 번
+}
+
+const url = "https://chatops-bot-test-20210226.taeuk.workers.dev/";
+const channelId = "814801307918925886";
+async function checkWebStatus(client) {
+	if (await isUp(url)) {
+		console.log("정상 작동중...");
+	} else {
+		console.log("웹사이트 비정상 작동!");
+		client.channels.cache.get(channelId).send("🤷‍♀️웹사이트 접속이 안 돼~");
+	}
 }
 
 export default onceReady;
